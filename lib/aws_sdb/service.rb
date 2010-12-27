098887c55623fdd48ba9e097c9d1e3d43b6a30f9
Dave M <dmarti21@gmail.com>
2010-12-27 01:51:30 +0000

    Created result format for select expressions and some minor clean up

--- lib/aws_sdb/service.rb
+++ lib/aws_sdb/service.rb
@@ -2,7 +2,6 @@ require 'logger'
 require 'time'
 require 'cgi'
 require 'uri'
-#require 'net/http'  #replaced with curb-fu for finer grain control
 require 'base64'
 require 'openssl'
 require 'rexml/document'
@@ -18,6 +17,7 @@ class CGI
       '%' + $1.unpack('H2' * $1.size).join('%').upcase
     end
   end
+  
 end
 
 module AwsSdb
@@ -57,48 +57,13 @@ module AwsSdb
       )
       nil
     end
-    
-    # <QueryWithAttributesResult><Item><Name>in-c2ffrw</Name><Attribute><Name>code</Name><Value>in-c2ffrw</Value></Attribute><Attribute><Name>date_created</Name><Value>2008-10-31</Value></Attribute></Item><Item>
-    def query_with_attributes(domain, query, max = nil, token = nil)
-      params = {
-        'Action' => 'QueryWithAttributes',
-        'QueryExpression' => query,
-        'DomainName' => domain.to_s
-      }
-      params['NextToken'] =
-        token unless token.nil? || token.empty?
-      params['MaxNumberOfItems'] =
-        max.to_s unless max.nil? || max.to_i == 0
-
-      doc = call(:get, params)
-      results = []
-      REXML::XPath.each(doc, "//Item") do |item|
-        name = REXML::XPath.first(item, './Name/text()').to_s
-
-
-        attributes = {'Name' => name}
-        REXML::XPath.each(item, "./Attribute") do |attr|
-          key = REXML::XPath.first(attr, './Name/text()').to_s
-          value = REXML::XPath.first(attr, './Value/text()').to_s
-          ( attributes[key] ||= [] ) << value
-        end
-        results << attributes
-      end
-      return results, REXML::XPath.first(doc, '//NextToken/text()').to_s
-    end
-
-    # <QueryResult><ItemName>in-c2ffrw</ItemName><ItemName>in-72yagt</ItemName><ItemName>in-52j8gj</ItemName>
-    
-    def select(query, max = nil, token = nil)
-      #domain is not needed
-      query #CGI.escape(query)
-      ####esc_query = aws_esc(query)
-      ###esc_query =  uri_esc(query)
+     
+    def select(query, max = nil, token = nil, consistent_read = 'true')
+      query 
       params = {
         'Action' => 'Select',
-        'SelectExpression' => query,##, #CGI.escape(query),
-        'ConsistentRead' => 'true'
-        #'DomainName' => domain.to_s
+        'SelectExpression' => query,
+        'ConsistentRead' => consistent_read
       }
       params['NextToken'] =
         token unless token.nil? || token.empty?
@@ -107,13 +72,30 @@ module AwsSdb
 
       @logger.debug { "SELECT EXPRESSION BEFORE CALL: #{query.inspect}" } if @logger.debug?
       doc = call(:get, params)
-      results = []
-      REXML::XPath.each(doc, '//Item/Name/text()') do |item|
-        results << item.to_s
+      item_doc = REXML::XPath.match(doc, '//Item')
+      items = {}
+      attributes = {}
+      
+      #build the result hash
+      REXML::XPath.each(item_doc, '//Item/Name') do |item_name_doc|
+        item_name = item_name_doc.text
+        REXML::XPath.each(item_doc, '//Attribute') do |attrib_doc|
+          attrib_name = nil
+          attrib_value = nil
+          attrib_doc.elements.each('Name') do |name|
+            attrib_name = CGI.unescape(name.text)
+          end
+          attrib_doc.elements.each('Value') do |value|
+            attrib_value = CGI.unescape(value.text)
+          end
+          add_value(attributes, attrib_name, attrib_value)
+        end
+        items[item_name] = attributes
       end
+      attr_names = REXML::XPath.match(item_doc, '//Attribute/Name/text()')
+      results = items
 
       return results, REXML::XPath.first(doc, '//NextToken/text()').to_s
-
     end
     
     def put_attributes(domain, item, attributes, replace = true)
@@ -126,8 +108,8 @@ module AwsSdb
       #escaping key and value so signature computes correctly
       attributes.each do | key, values |
         ([]<<values).flatten.each do |value|
-          params["Attribute.#{count}.Name"] = CGI.escape(key.to_s) ##key.to_s #CGI.escape(key.to_s)
-          params["Attribute.#{count}.Value"] = CGI.escape(value.to_s) ##value.to_s #CGI.escape(value.to_s)
+          params["Attribute.#{count}.Name"] = CGI.escape(key.to_s)
+          params["Attribute.#{count}.Value"] = CGI.escape(value.to_s)
           params["Attribute.#{count}.Replace"] = replace
           count += 1
         end
@@ -137,14 +119,14 @@ module AwsSdb
     end
 
     #updated to support consitent read
-    def get_attributes(domain, item, consist_read = 'true')
+    def get_attributes(domain, item, consistent_read = 'true')
       doc = call(
         :get,
         {
           'Action' => 'GetAttributes',
           'DomainName' => domain.to_s,
           'ItemName' => item.to_s,
-          'ConsistentRead' => 'true'
+          'ConsistentRead' =>  consistent_read
         }
       )
       attributes = {}
@@ -152,8 +134,8 @@ module AwsSdb
         unesc_key = REXML::XPath.first(attr, './Name/text()').to_s
         unesc_value = REXML::XPath.first(attr, './Value/text()').to_s
         #unescape key and value to return to normal
-        key = CGI.unescape(unesc_key)#aws_unesc(unesc_key) ###CGI.unescape(unesc_key) ##unesc_key #CGI.unescape(unesc_key)
-        value = CGI.unescape(unesc_value)#aws_unesc(unesc_value) ###CGI.unescape(unesc_value) ##unesc_value #CGI.unescape(unesc_value)
+        key = CGI.unescape(unesc_key)
+        value = CGI.unescape(unesc_value)
         ( attributes[key] ||= [] ) << value
       end
       attributes
@@ -174,31 +156,11 @@ module AwsSdb
     protected
     
     def build_canonical_query_string(q_params)
-      qs = []
-      q_params.sort.each do |k,v|
-        if nil #k == "SelectExpression" 
-          new_v = [k.to_s, v.to_s].join('=').gsub('+', '%20')
-          puts "New V: #{new_v.inspect}"
-          qs << new_v
-        else
-          #puts "OKC"
-          qs << [CGI.escape(k.to_s), CGI.escape(v.to_s)].join('=')
-        end
-      end
-      cq_string = qs.join('&')
+      q_params.sort.collect { |k,v| [CGI.escape(k.to_s), CGI.escape(v.to_s)].join('=')}.join('&')
     end
 
     def build_actual_query_string(q_params)
-      qs = []
-      q_params.sort.each do |k,v|
-        if nil #k == "SelectExpression" 
-          qs << [k.to_s, v.to_s].join('=')
-        else
-          #puts "OKA"
-          qs << [CGI.escape(k.to_s), CGI.escape(v.to_s)].join('=')
-        end
-      end
-      aq_string = qs.join('&')
+      q_params.sort.collect { |k,v| [CGI.escape(k.to_s), CGI.escape(v.to_s)].join('=')}.join('&')
     end
 
     def call(method, params)
@@ -212,18 +174,11 @@ module AwsSdb
           'Timestamp' => Time.now.gmtime.iso8601
         }
       )
-  
-
-
       
       @logger.debug { "CALL: #{method} with #{params.inspect}" } if @logger.debug?
       
   
       canonical_querystring = build_canonical_query_string(params)
-      ##canonical_querystring = params.sort.collect { |k,v| [CGI.escape(k.to_s), CGI.escape(v.to_s)].join('=')}.join('&')
-      ##canonical_querystring = params.sort.collect { |k,v| [k.to_s, v.to_s].join('=')}.join('&')
-      ####canonical_querystring = params.sort.collect { |k,v| [aws_esc(k.to_s), aws_esc(v.to_s)].join('=')}.join('&')
-
       
       @logger.debug { "CANONICAL: #{canonical_querystring.inspect}" } if @logger.debug?
       
@@ -238,9 +193,6 @@ module AwsSdb
       #signature = Base64.encode64(OpenSSL::HMAC.digest(digest, @secret_access_key, string_to_sign)).chomp      
       
       params['Signature'] = signature
-      ###querystring = params.collect { |key, value| [CGI.escape(key.to_s), CGI.escape(value.to_s)].join('=') }.join('&') # order doesn't matter for the actual request
-      ##querystring = params.collect { |key, value| [key.to_s, value.to_s].join('=') }.join('&') # order doesn't matter for the actual request
-      ####querystring = params.sort.collect { |key, value| [aws_esc(key.to_s), aws_esc(value.to_s)].join('=') }.join('&') # order doesn't matter for the actual request
       querystring = build_actual_query_string(params)
       
       @logger.debug { "ACTUALQUERY: #{querystring.inspect}" } if @logger.debug?
@@ -248,22 +200,16 @@ module AwsSdb
       url = "#{@base_url}?#{querystring}"
       uri = URI.parse(url)
 
-      #resp = CurbFu.get(url)
       resp = request(url)
       resp_body = resp.body
-      #resp_body =  `curl -X"GET" "#{url}" -A"simple ruby aws sdb wrapper"`
       
       @logger.debug { "RESP: #{resp_body.inspect}" } if @logger.debug?
-      #puts "RESP: #{resp.body.inspect}"
  
       doc = REXML::Document.new(resp_body)
       
       @logger.debug { "DECODED DOC:" } if @logger.debug?
-      #@logger.debug {doc.elements.each {|e| e.to_s } } if @logger.debug?
       
       error = doc.get_elements('*/Errors/Error')[0]
-      #puts "ERROR: #{error.to_s}"
-      #puts "ERROR: #{error.get_elements('Code')[0].text}" if error
       
       raise(
         Module.class_eval(
@@ -276,10 +222,9 @@ module AwsSdb
       doc
     end
     
+    #request with retries
     def request(url, retry_data={})
       resp = CurbFu.get(url) 
-      #puts "RESP HEAD: #{ resp.headers.inspect }"
-      puts "RESP STATUS: #{resp.status.inspect}"
       if resp.nil? || resp.status == 503
         resp = retry_req(url, retry_data)
       end
@@ -287,6 +232,8 @@ module AwsSdb
       raise "No Body in Response" unless resp.body
       resp
     end
+    
+    private
     
     def retry_req(url, retry_data)
       resp = :retry
@@ -308,6 +255,16 @@ module AwsSdb
       end
       resp
     end
-  end
+    
+    def add_value(attributes, attrib_name, attrib_value)
+      if attributes[attrib_name]
+        attributes[attrib_name] << attrib_value
+      else
+        attributes[attrib_name] = [attrib_value]
+      end
+      attributes
+    end
 
-end
+  end#class
+
+end#module
